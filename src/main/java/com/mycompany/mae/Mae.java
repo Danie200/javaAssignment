3f50a1ae722a5e4672c5c020f1de4c546d934f9c
Daniel Richard <drekwere5@gmail.com>
2025-01-23 18:28:42 +0100

    assignment

---
+++ src/main/java/com/mycompany/mae/Mae.java
@@ -0,0 +1,21 @@
+package com.mycompany.mae;
+
+
+public class Mae {
+
+    public static void main(String[] args) {
+        for(int z = 0; z<4; z++){
+            System.out.println();
+            for(int a = 0; a < 4; a++){
+                System.out.print("*");
+            }
+            for(int b = 0; b < 4; b++){
+                System.out.print("=");
+            }
+            for(int c = 0; c < 4; c++){
+                System.out.print("*");
+            }
+            
+        }
+    }
+}
