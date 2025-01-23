56007dca02a23ec65d930f83abae41e40491f560
Daniel Richard <drekwere5@gmail.com>
2025-01-23 22:16:17 +0100

    assignment

---
+++ src/main/java/com/mycompany/mae/question4.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.mae;
+import java.util.Scanner;
+
+public class question4 {
+    public static void main(String[] args) {
+         int[] array = new int[10];
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("for index " + i + " enter a number:");
+            array[i] = input.nextInt();
+        }
+
+        for (int x : array) {
+            System.out.println(x);
+        }
+    }
+}
