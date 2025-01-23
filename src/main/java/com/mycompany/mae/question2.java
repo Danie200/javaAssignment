9a28c73258dc98cdc6749e81372a191e63f2f975
Daniel Richard <drekwere5@gmail.com>
2025-01-23 22:10:12 +0100

    assignment

---
+++ src/main/java/com/mycompany/mae/question2.java
@@ -0,0 +1,26 @@
+
+package com.mycompany.mae;
+
+public class question2 {
+     public static void main(String[] args) {
+       for(int z = 0; z < 3; z++){
+           System.out.println();
+           for(int a = 0; a < 4; a++){
+              System.out.print("*");
+           }
+           for(int b = 0; b < 7; b++){
+               System.out.print("=");
+           }
+       }
+       for (int y = 0; y < 3; y++){
+           System.out.println();
+           for(int c = 0; c < 5; c++){
+               System.out.print("=");
+           }
+           System.out.print(" ");
+           for(int d = 0; d < 5; d++){
+               System.out.print("=");
+           }
+       }
+    }
+}
